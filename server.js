070230117dc100298387dd6e05da3867ed7e14b8
yohanjoseph <yohanjoseph@gmail.com>
2020-08-14 14:19:15 +0530

    Update connection string

--- server.js
+++ server.js
@@ -11,8 +11,12 @@ const image = require('./controllers/image');
 
 const db = knex({
   client: 'pg',
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
+  connection:{
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }
 });
 
 const app = express();
